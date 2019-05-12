f6c2e69498aed2b84d9f32323015a679858108ee
showtara <shoara0509@gmail.com>
2019-05-12 14:40:53 +0900

    Nimファイルのアップロード

---
+++ Nim/cell.nim
@@ -0,0 +1,57 @@
+import sequtils
+
+type CellState* = enum
+    DEAD
+    ALIVE
+
+type Cell* = ref object
+    x: int
+    y: int
+    nextState: CellState
+    currentState: CellState
+
+method getX*(cell: Cell): int {.base.} = cell.x
+method getY*(cell: Cell): int {.base.} = cell.y
+
+method setCurrentState*(cell: Cell, state: CellState) {.base.} =
+    cell.currentState = state
+    
+
+method getStateMark*(cell: Cell): string {.base.} = 
+    result = case cell.currentState:
+        of ALIVE:
+            "■ "
+        else:
+            "□ "
+
+method toNextGen*(cell: Cell) {.base.} =
+    cell.currentState = cell.nextState
+
+method init*(cell: Cell, x: int, y: int, currentState: CellState) {.base.} = 
+    cell.x = x
+    cell.y = y
+    cell.currentState = currentState
+
+method prepareCell*(cell: Cell, aroundCells: seq[Cell]) {.base.} =
+    let aliveCount = aroundCells.filterIt(it.currentState == ALIVE).len()
+    if cell.currentState == ALIVE:
+        cell.nextState = case aliveCount:
+            of 2, 3:
+                ALIVE
+            else:
+                DEAD
+    else:
+        cell.nextState = case aliveCount:
+            of 3:
+                ALIVE
+            else:
+                DEAD
+
+method isOnTop*(cell: Cell) : bool {.base.} = cell.y == 0
+method isOnBottom*(cell: Cell, boardSize: int) : bool {.base.} = cell.y == boardSize - 1
+method isOnStart*(cell: Cell) : bool {.base.} = cell.x == 0
+method isOnEnd*(cell: Cell, boardSize: int): bool {.base.} = cell.x == boardSize - 1
+
+let cell = Cell(nextState: DEAD, currentState: ALIVE)
+cell.toNextGen()
+echo cell.getStateMark
---
+++ Nim/gameboard.nim
@@ -0,0 +1,129 @@
+import cell as cl
+import utils
+import sequtils, strutils, math, algorithm
+
+type Gameboard* = ref object
+    sideLen*: int
+    offset*: int
+    boardArray: seq[seq[Cell]]
+
+method getBoardArray*(gameboard: Gameboard): seq[seq[Cell]] {.base.} =
+    gameboard.boardArray
+
+method getCellOfRelationalPosition(gameboard: Gameboard, cell: Cell, positions: seq[int]): seq[Cell] {.base.} =
+    let 
+        board = gameboard.boardArray
+        x = cell.getX()
+        y = cell.getY()
+    
+    result = positions.mapIt(
+        case it:
+            of 1:
+                board[y - 1][x - 1]
+            of 2:
+                board[y - 1][x]
+            of 3:
+                board[y - 1][x + 1]
+            of 4:
+                board[y][x - 1]
+            of 5:
+                board[y][x + 1]
+            of 6:
+                board[y + 1][x - 1]
+            of 7:
+                board[y + 1][x]
+            of 8:
+                board[y + 1][x + 1]
+            else:
+                raise newException(IndexError, "第二引数はseq[int]で1~8の値を持つものにしてください")
+    )
+
+method getAroundCells(gameboard: Gameboard, cell: Cell): seq[Cell] {.base.} =
+    var aroundPositions: seq[int]
+    let
+        isOnTop = cell.isOnTop()
+        isOnBottom = cell.isOnBottom(gameboard.sideLen)
+        isOnStart = cell.isOnStart()
+        isOnENd = cell.isOnEnd(gameboard.sideLen)
+
+    if isOnStart and isOnTop:
+        aroundPositions = @[5, 7, 8]
+    elif isOnStart and isOnBottom:
+        aroundPositions = @[2, 3, 5]
+    elif isOnEnd and isOnTop:
+        aroundPositions = @[4, 6, 7]
+    elif isOnEnd and isOnBottom:
+        aroundPositions = @[1, 2, 4]
+    elif isOnStart:
+        aroundPositions = @[2, 3, 5, 7, 8]
+    elif isOnEnd:
+        aroundPositions = @[1, 2, 4, 6, 7]
+    elif isOnTop:
+        aroundPositions = @[4, 5, 6, 7, 8]
+    elif isOnBottom:
+        aroundPositions = @[1, 2, 3, 4, 5]
+    else:
+        aroundPositions = @[1, 2, 3, 4, 5, 6, 7, 8]
+
+    gameboard.getCellOfRelationalPosition(cell, aroundPositions)
+
+method toNextGen*(gameboard: Gameboard) {.base.} =
+    forEachDubleArray[Cell](gameboard.boardArray, proc(cell: Cell) = cell.prepareCell(gameboard.getAroundCells(cell)))
+    forEachDubleArray[Cell](gameboard.boardArray, proc(cell: Cell) = cell.toNextGen())
+
+method createBreeder(gameboard: Gameboard, x: int, y:int, offset: int): CellState {.base.} =
+    var state: CellState
+    if x == 1 + offset and y == 1 + offset:
+        state = ALIVE
+    elif x == 2 + offset and y == 1 + offset:
+        state = ALIVE
+    elif x == 3 + offset and y == 1 + offset:
+        state = ALIVE
+    elif x == 5 + offset and y == 1 + offset:
+        state = ALIVE
+    elif x == 1 + offset and y == 2 + offset:
+        state = ALIVE
+    elif x == 4 + offset and y == 3 + offset:
+        state = ALIVE
+    elif x == 5 + offset and y == 3 + offset:
+        state = ALIVE
+    elif x == 2 + offset and y == 4 + offset:
+        state = ALIVE
+    elif x == 3 + offset and y == 4 + offset:
+        state = ALIVE
+    elif x == 5 + offset and y == 4 + offset:
+        state = ALIVE
+    elif x == 1 + offset and y == 5 + offset:
+        state = ALIVE
+    elif x == 3 + offset and y == 5 + offset:
+        state = ALIVE
+    elif x == 5 + offset and y == 5 + offset:
+        state = ALIVE
+    else:
+        state = DEAD
+
+    return state
+        
+
+method onCreate*(gameboard: Gameboard) {.base.} =
+    echo "Board Ready"
+    # インスタンスのnew時に呼ぶこと
+    # TODO: ライフゲームの初期状態の設定 
+    let size = gameboard.sideLen
+    if size == 0:
+        return
+    
+    gameboard.boardArray = newSeqWith(size, newSeqWith(size, Cell()))
+    forEachIndexed[seq[Cell]](
+        gameboard.boardArray,
+        proc(row: seq[Cell], y: int) = 
+            forEachIndexed[Cell](
+                row,
+                proc (cell: Cell, x: int) =
+                    cell.init(
+                        x,
+                        y,
+                        gameboard.createBreeder(x, y, gameboard.offset)
+                    )
+            )
+    )
